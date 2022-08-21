c61c21130c1c535d82356ee20970f66afa80bdc7
unknown <emmakanebi@gmail.com>
2022-08-20 19:07:28 -0700

    sls

--- kaneweb/settings.py
+++ kaneweb/settings.py
@@ -113,10 +113,15 @@ WSGI_APPLICATION = 'kaneweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
