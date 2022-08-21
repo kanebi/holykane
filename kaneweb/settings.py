c1a85168df51f5eecbc7bbc256cf418a9f85e7ec
unknown <emmakanebi@gmail.com>
2022-08-20 19:32:54 -0700

    jsjs7d

--- kaneweb/settings.py
+++ kaneweb/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = [".herokuapp.com", '127.0.0.1']
+ALLOWED_HOSTS = [".herokuapp.com", '127.0.0.1',
+                 'https://holykane.herokuapp.com/']
 
 
 # Application definition
