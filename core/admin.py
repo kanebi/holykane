bb6e1204de2479787cf77c02167ceaf06916248f
unknown <emmakanebi@gmail.com>
2022-08-22 10:35:18 -0700

    jsjsksfd980

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Slides, HomePage, ContactPage, ContactUs, UserProfile
+from .models import Slides, HomePage, ContactPage, ContactUs, UserProfile, Header
 # Register your models here.
 
 
